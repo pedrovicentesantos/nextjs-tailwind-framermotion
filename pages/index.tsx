b4b12372aeb352ae975b1038107df05b61ef591a
Pedro Vicente <pedrovicente@poli.ufrj.br>
2021-01-17 20:41:47 -0300

    Fix index h1 text and position

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <div>
       <motion.h1
-        className="text-4xl mt-10 leading-none font-extrabold tracking-tight text-gray-900 sm:text-6xl lg:text-7xl md:mt-24"
+        className="text-center text-4xl mt-10 leading-none font-extrabold tracking-tight text-gray-900 sm:text-6xl lg:text-7xl md:mt-24"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 2 }}
       >
-        Construa sites modernos sem precisar sair do HTML.
+        Construa sites modernos sem precisar sair do HTML
       </motion.h1>
       <motion.div
         className="w-full grid grid-cols-1 grid-rows-3 gap-4 mt-10 md:grid-cols-3 md:grid-rows-1 md:mt-24"
